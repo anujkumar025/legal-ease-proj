010630c49fd1f1575f9ef55a0b0940bb0069b96e
Aditya Sharma <3008.aditya.sharma@gmail.com>
2023-11-04 13:03:36 +0530

    initial commit

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -34,9 +34,9 @@ const Header = () => {
               <Link to='/contact'>Contact</Link>
             </li>
           </ul>
-          <div className='start'>
+          {/* <div className='start'>
             <div className='button'>GET CERTIFICATE</div>
-          </div>
+          </div> */}
           <button className='toggle' onClick={() => setClick(!click)}>
             {click ? <i className='fa fa-times'> </i> : <i className='fa fa-bars'></i>}
           </button>
